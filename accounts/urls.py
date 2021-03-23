1c5ab06968f81c21ed82c5fc85a0641aa82b1106
joe <joelibral2693@gmail.com>
2021-03-23 20:32:51 +0430

    class base views for accounts complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views#path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-from . import views
-
+from .views import(
+        PasswordConfirm, UserPassReset, UserLogin,
+        PasswordResetDone, PasswordResetComplete
+)
 app_name = 'accounts'
 
 
 urlpatterns =[
-    path('login/', views.UserLogin.as_view(), name='login'),
+    path('login/', UserLogin.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset/', UserPassReset.as_view(), name='reset'),
+    path('reset/done/', PasswordResetDone.as_view(), name = 'password-reset-done'),
+    path('confirm/<uidb64>/<token>/', PasswordConfirm.as_view(), name = 'password-confirm'),
+    path('confirm/done/', PasswordResetComplete.as_view(), name = 'resaet-done'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 # Create your views here.
+from django.urls import reverse_lazy
 from django.contrib.auth.views import (
-        LoginView, LogoutView
+        LoginView, LogoutView, PasswordResetView,
+        PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
     )
 from first.models import Todo
 
@@ -10,3 +12,21 @@ class UserLogin(LoginView):
     users = User.objects.all()
     extra_context = {'users':users}
     template_name = 'accounts/login.html'
+
+
+class UserPassReset(PasswordResetView):
+    template_name = 'accounts/password_reset_form.html'
+    success_url = reverse_lazy('acccounts:password-reset-done')
+    html_email_template_name = 'accounts/password_reset_email.html'
+
+
+class PasswordResetDone(PasswordResetDoneView):
+    template_name='accounts/reset_done.html'
+
+
+class PasswordConfirm(PasswordResetConfirmView):
+    template_name = 'accounts/password_reset_confirm.html'
+
+
+class PasswordResetComplete(PasswordResetCompleteView):
+    template_name = 'accounts/password_reset_complete.html'
